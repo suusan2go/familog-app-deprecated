bb56431c5b733afe3379a738067b6a8942c12c25
suzan2go <ksuzuki180@gmail.com>
2017-05-20 00:31:25 +0900

    handling errors

--- containers/DiaryEntryList.js
+++ containers/DiaryEntryList.js
@@ -24,25 +24,35 @@ export default connect(
         const { currentDiary, sessionToken, diaryEntryList } = store.getState();
         if (currentDiary === null) return;
         const Api = new ApiClient(store.getState().sessionToken);
-        const newDiaryEntryList = await Api.getMoreNewerDiaryEntries(
-          currentDiary.id,
-          diaryEntryList.diaryEntries[0].id,
-        );
-        dispatch(Actions.unshiftDiaryEntryList(newDiaryEntryList.diaryEntries));
-        dispatch(Actions.getDiaryEntryListSuccess());
+        try {
+          const newDiaryEntryList = await Api.getMoreNewerDiaryEntries(
+            currentDiary.id,
+            diaryEntryList.diaryEntries[0].id,
+          );
+          dispatch(
+            Actions.unshiftDiaryEntryList(newDiaryEntryList.diaryEntries),
+          );
+          dispatch(Actions.getDiaryEntryListSuccess());
+        } catch (error) {
+          dispatch(Actions.getDiaryEntryListFailure());
+        }
       },
       loadMoreOlderDiaryEntries: async () => {
         dispatch(Actions.getDiaryEntryListStart());
         const { currentDiary, sessionToken, diaryEntryList } = store.getState();
         if (currentDiary === null) return;
         const Api = new ApiClient(store.getState().sessionToken);
-        const newDiaryEntryList = await Api.getMoreOlderDiaryEntries(
-          currentDiary.id,
-          diaryEntryList.diaryEntries[diaryEntryList.diaryEntries.length - 1]
-            .id,
-        );
-        dispatch(Actions.pushDiaryEntryList(newDiaryEntryList.diaryEntries));
-        dispatch(Actions.getDiaryEntryListSuccess());
+        try {
+          const newDiaryEntryList = await Api.getMoreOlderDiaryEntries(
+            currentDiary.id,
+            diaryEntryList.diaryEntries[diaryEntryList.diaryEntries.length - 1]
+              .id,
+          );
+          dispatch(Actions.pushDiaryEntryList(newDiaryEntryList.diaryEntries));
+          dispatch(Actions.getDiaryEntryListSuccess());
+        } catch (error) {
+          dispatch(Actions.getDiaryEntryListFailure());
+        }
       },
     },
   }),
