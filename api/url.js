b8b5a2133dbec7352cf773503d832d7c360e6e30
suzan2go <ksuzuki180@gmail.com>
2017-05-20 00:25:51 +0900

    change localhost uri

--- api/url.js
+++ api/url.js
@@ -1,6 +1,6 @@
 /* @flow */
 import Config from '../config.json';
-const HOST = __DEV__ ? 'http://192.168.1.6:8080' : Config.endpoint;
+const HOST = __DEV__ ? 'http://192.168.1.4:8080' : Config.endpoint;
 
 export const DEVICE_URL = `${HOST}/device`;
 export const SESSION_URL = `${HOST}/session`;
