b859fa4ed6795504dc43d42d813d7c3c58b7ef4c
suzan2go <ksuzuki180@gmail.com>
2017-05-30 00:01:13 +0900

    add bugsnap integration for crash reporting

--- App.js
+++ App.js
@@ -5,6 +5,14 @@ import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import MainScreen from './containers/MainScreen.js';
 import store from './store';
+import AppConfig from './config.json';
+import { Client, Configuration } from 'bugsnag-react-native';
+
+if (!__DEV__) {
+  const configuration = new Configuration();
+  configuration.apiKey = AppConfig.bugsnapKey;
+  const client = new Client(configuration);
+}
 
 const App = () => (
   <Provider store={store}>
